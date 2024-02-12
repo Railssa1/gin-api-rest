d21440fdf2ca4b8098c48a684921e343a6e4ccd0
Railssa1 <81435302+Railssa1@users.noreply.github.com>
2024-02-11 21:50:14 -0300

    Add files via upload

---
+++ controllers/controller.go
@@ -0,0 +1,87 @@
+package controllers
+
+import (
+	"api-go-gin/database"
+	"api-go-gin/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ExibeTodosAlunos(c *gin.Context) {
+	var alunos []models.Aluno
+	database.DB.Find(&alunos)
+	c.JSON(http.StatusOK, alunos)
+}
+
+func Saudacao(c *gin.Context) {
+	nome := c.Params.ByName("nome")
+	c.JSON(http.StatusOK, gin.H{
+		"API diz: ": "Bem-vindo(a) " + nome,
+	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusCreated, aluno)
+}
+
+func RecuperaAlunoPorId(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.First(&aluno, id)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not Found": "Aluno não encontrado",
+		})
+	} else {
+		c.JSON(http.StatusOK, &aluno)
+	}
+}
+
+func DeletaAluno(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.Delete(&aluno, id)
+
+	c.JSON(http.StatusOK, "Aluno deletado com sucesso!")
+}
+
+func EditaAluno(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.First(&aluno, id)
+
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not Found": "Aluno não encontrado",
+		})
+		return
+	}
+
+	database.DB.Model(&aluno).UpdateColumns(aluno)
+	c.JSON(http.StatusOK, aluno)
+}
+
+func RecuperaAlunoPorCpf(c *gin.Context) {
+	var aluno models.Aluno
+	cpf := c.Param("cpf")
+	database.DB.Where(&models.Aluno{CPF: cpf}).First(&aluno)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not Found": "Aluno não encontrado",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, aluno)
+}
